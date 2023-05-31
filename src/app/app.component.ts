8771075b3ef1174b51f4307ea6c63588e3e0ee17
matheus jordan <matheusj1599@gmail.com>
2023-05-31 14:46:05 -0300

    Added stock form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {SharedMaterialModule} from "./shared/shared-material.module";
 import {SharedComponentsModule} from "./shared/components/shared-components.module";
+import {MatDialog} from "@angular/material/dialog";
+import {DetailedStockComponent} from "./shared/components/detailed-stock/detailed-stock.component";
 
 @Component({
   selector: 'mjx-root',
@@ -12,5 +14,18 @@ import {SharedComponentsModule} from "./shared/components/shared-components.modu
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'stock';
+  title = 'Estoque App';
+
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    const dialogRef = this.dialog.open(DetailedStockComponent, {
+      width: '500px',
+      height: 'auto'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
--- src/app/shared/shared-material.module.ts
+++ src/app/shared/shared-material.module.ts
@@ -7,6 +7,7 @@ import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatTableModule} from '@angular/material/table';
+import {MatDialogModule} from '@angular/material/dialog';
 
 @NgModule({
   imports: [
@@ -19,7 +20,8 @@ import {MatTableModule} from '@angular/material/table';
     MatInputModule,
     NgFor,
     AsyncPipe,
-    MatTableModule
+    MatTableModule,
+    MatDialogModule
   ],
   exports: [
     MatToolbarModule,
@@ -30,7 +32,8 @@ import {MatTableModule} from '@angular/material/table';
     MatInputModule,
     NgFor,
     AsyncPipe,
-    MatTableModule
+    MatTableModule,
+    MatDialogModule
   ]
 })
 export class SharedMaterialModule { }
