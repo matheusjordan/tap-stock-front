03b33043dad55627b350888d79d15ecb2495f8a1
matheus jordan <matheusj1599@gmail.com>
2023-05-31 14:12:32 -0300

    added initial application bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {SharedMaterialModule} from "./shared/shared-material.module";
+
 
 @Component({
   selector: 'mjx-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, SharedMaterialModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
